607e6a3fb59a0266f74158f4f40b5803b0badddb
Ivan Rostovsky <ir@anahoret.com>
2012-06-06 16:52:03 +0300

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Artlife::Application.config.secret_token = 'adaa6be2383700a965aeff10e09eb8e5f11b803ab5fdff0243e9aa4d3f2144bef4f48ca04d2f9da256d293734809c214986f44118f9d3a8f0a9d21e805ffcbec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Artlife::Application.config.session_store :cookie_store, key: '_artlife_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Artlife::Application.config.session_store :active_record_store
