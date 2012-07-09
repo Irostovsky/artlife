fec1801d6ada647119e8d730083639a7269bdc78
Bakuta Andrey <dra1n86@gmail.com>
2012-07-10 00:19:50 +0300

    Added styles for header, top_menu and bottom_menu.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def locale_switcher(locale)
     if locale == I18n.locale
-      locale.upcase
+      content_tag('span', locale.upcase)
     else
       link_to locale.upcase, locale_path(locale), :method => :put
     end
